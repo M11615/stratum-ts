18c41810248403f35375eb03f5fbe46dae59a4e5
M11615 <M11615@outlook.com>
2025-09-03 17:23:16 +0800

    Type optimisation

--- client/src/app/[lng]/page.tsx
+++ client/src/app/[lng]/page.tsx
@@ -1,10 +1,10 @@
-import { i18n } from "i18next";
 import { getT } from "@/app/i18n/index";
+import { OptionalI18n } from "@/app/lib/constants";
 import { getHello } from "@/app/services/v1/app";
 import Page from "./components/Page";
 
 export default async function Home(): Promise<React.ReactNode> {
-  const { i18n }: { i18n: i18n } = await getT("", [null, ""]);
+  const { i18n }: OptionalI18n = await getT("", [null, ""]);
   const response: Response = await getHello(i18n);
   if (response.ok) {
     const requestBody: string = await response.text();
