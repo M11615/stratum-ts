d96f74959e7cc4fe90aa47fdfe46cb8caaccf0e6
M11615 <M11615@outlook.com>
2025-09-05 12:42:40 +0800

    Delete initial interface test

--- client/src/app/[lng]/page.tsx
+++ client/src/app/[lng]/page.tsx
@@ -1,16 +1,6 @@
-import { getT } from "@/app/i18n/index";
-import { OptionalI18n } from "@/app/lib/constants";
-import { getHello } from "@/app/services/v1/app";
 import Page from "./components/Page";
 
 export default async function Home(): Promise<React.ReactNode> {
-  const { i18n }: OptionalI18n = await getT("", [null, ""]);
-  const response: Response = await getHello(i18n);
-  if (response.ok) {
-    const requestBody: string = await response.text();
-    console.log(requestBody);
-  }
-
   return (
     <Page />
   );
