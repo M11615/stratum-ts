cc90ccdba04591228b476926a381621aefad7f39
M11615 <M11615@outlook.com>
2025-10-09 13:23:16 +0800

    Change the font to a web font

--- client/src/app/[lng]/layout.tsx
+++ client/src/app/[lng]/layout.tsx
@@ -1,4 +1,4 @@
-import localFont from "next/font/local";
+import { Geist, Geist_Mono } from "next/font/google";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
 import { NextFontWithVariable } from "next/dist/compiled/@next/font";
@@ -8,14 +8,14 @@ import { OptionalI18n, COOKIE_KEYS, FALLBACK_THEME } from "@/app/lib/constants";
 import ResponsiveProvider from "./components/ResponsiveContext";
 import "./globals.css";
 
-const geistSans: NextFontWithVariable = localFont({
-  src: "../fonts/Geist/Geist-VariableFont_wght.ttf",
-  variable: "--font-geist-sans"
+const geistSans: NextFontWithVariable = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
 });
 
-const geistMono: NextFontWithVariable = localFont({
-  src: "../fonts/Geist_Mono/GeistMono-VariableFont_wght.ttf",
-  variable: "--font-geist-mono"
+const geistMono: NextFontWithVariable = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
 });
 
 export async function generateMetadata(): Promise<Metadata> {
