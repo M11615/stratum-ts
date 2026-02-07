3fd4e2e56572fbe267c393e0270eb3ef6ffba4aa
M11615 <yiyangyan9@gmail.com>
2026-02-07 13:40:15 +0800

    Ensure that the client URL accessed by the Next.js application always points to itself.

--- client/src/app/services/v1/app.ts
+++ client/src/app/services/v1/app.ts
@@ -2,7 +2,7 @@ import { headerName } from "@/app/i18n/settings";
 import { i18n } from "i18next";
 
 export const getHello = async (i18n: i18n): Promise<Response> => {
-  const requestUrl: URL = new URL("api/v1/app/get-hello", process.env.NEXT_PUBLIC_CLIENT_URL);
+  const requestUrl: URL = new URL("api/v1/app/get-hello");
   const requestHeader: Headers = new Headers({
     "Content-Type": "text/plain; charset=utf-8",
     [headerName]: i18n.language
--- client/src/app/services/v1/subscription.ts
+++ client/src/app/services/v1/subscription.ts
@@ -1,7 +1,7 @@
 import { SubscriptionRequest } from "@/app/api/v1/subscription/create-subscription/route";
 
 export const createSubscription = async (requestBody: SubscriptionRequest): Promise<Response> => {
-  const requestUrl: URL = new URL("api/v1/subscription/create-subscription", process.env.NEXT_PUBLIC_CLIENT_URL);
+  const requestUrl: URL = new URL("api/v1/subscription/create-subscription");
   const requestHeader: Headers = new Headers({
     "Content-Type": "application/json; charset=utf-8"
   });
