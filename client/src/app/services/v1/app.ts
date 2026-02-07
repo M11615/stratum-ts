6da7856c509a936dd894c2430c9c910234056e62
M11615 <yiyangyan9@gmail.com>
2026-02-07 13:47:30 +0800

    Ensure that the client URL accessed by the Next.js application always points to itself.

--- client/src/app/services/v1/app.ts
+++ client/src/app/services/v1/app.ts
@@ -2,12 +2,11 @@ import { headerName } from "@/app/i18n/settings";
 import { i18n } from "i18next";
 
 export const getHello = async (i18n: i18n): Promise<Response> => {
-  const requestUrl: URL = new URL("api/v1/app/get-hello");
   const requestHeader: Headers = new Headers({
     "Content-Type": "text/plain; charset=utf-8",
     [headerName]: i18n.language
   });
-  const response: Response = await fetch(requestUrl.toString(), {
+  const response: Response = await fetch("api/v1/app/get-hello", {
     method: "GET",
     headers: requestHeader,
     credentials: "include"
--- client/src/app/services/v1/subscription.ts
+++ client/src/app/services/v1/subscription.ts
@@ -1,11 +1,10 @@
 import { SubscriptionRequest } from "@/app/api/v1/subscription/create-subscription/route";
 
 export const createSubscription = async (requestBody: SubscriptionRequest): Promise<Response> => {
-  const requestUrl: URL = new URL("api/v1/subscription/create-subscription");
   const requestHeader: Headers = new Headers({
     "Content-Type": "application/json; charset=utf-8"
   });
-  const response: Response = await fetch(requestUrl.toString(), {
+  const response: Response = await fetch("api/v1/subscription/create-subscription", {
     method: "POST",
     headers: requestHeader,
     body: JSON.stringify(requestBody)
